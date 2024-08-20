9fb95b27375c1bfaee1a800786f290bf1afbeb2f
Ushuaia Contenidos <agupeiretti@gmail.com>
2024-08-20 10:56:52 -0300

    nueva peer constante

--- src/peerService.js
+++ src/peerService.js
@@ -1,7 +1,7 @@
 import Peer from "peerjs";
 
 // Crea una instancia de Peer utilizando un servidor gratuito de PeerJS
-const peer = new Peer({
+const peer = new Peer("custom-unique-id", {
   host: "0.peerjs.com",
   port: 443,
   secure: true,
