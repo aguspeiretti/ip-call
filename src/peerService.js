48d3cc1acbcf02925fbe6902d81816ed5d034791
Ushuaia Contenidos <agupeiretti@gmail.com>
2024-08-20 11:01:55 -0300

    nueva peer id unico

--- src/peerService.js
+++ src/peerService.js
@@ -1,7 +1,15 @@
 import Peer from "peerjs";
+import { v4 as uuidv4 } from "uuid";
+
+let peerId = localStorage.getItem("peerId");
+
+if (!peerId) {
+  peerId = uuidv4(); // Genera un nuevo ID único
+  localStorage.setItem("peerId", peerId);
+}
 
 // Crea una instancia de Peer utilizando un servidor gratuito de PeerJS
-const peer = new Peer("custom-unique-id", {
+const peer = new Peer(peerId, {
   host: "0.peerjs.com",
   port: 443,
   secure: true,
