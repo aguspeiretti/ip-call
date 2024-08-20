7b8bcd01920e08b1d8080054b24df80b84f2fef5
Ushuaia Contenidos <agupeiretti@gmail.com>
2024-08-20 10:30:36 -0300

    nueva prueba peer

--- src/peerService.js
+++ src/peerService.js
@@ -2,18 +2,9 @@ import Peer from "peerjs";
 
 // Crea una instancia de Peer utilizando un servidor gratuito de PeerJS
 const peer = new Peer({
-  host: "peerjs-server.herokuapp.com",
+  host: "0.peerjs.com",
   port: 443,
   secure: true,
-  config: {
-    iceServers: [
-      { url: "stun:stun.l.google.com:19302" },
-      { url: "stun:stun1.l.google.com:19302" },
-    ],
-  },
-  fetch: {
-    mode: "no-cors",
-  },
 });
 
 export const createPeer = () => peer;
