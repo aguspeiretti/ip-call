7a42cc2ced32f96bdddf7b3d7ec854eae136e6c6
Ushuaia Contenidos <agupeiretti@gmail.com>
2024-08-20 10:27:29 -0300

    nueva peer

--- src/peerService.js
+++ src/peerService.js
@@ -5,6 +5,15 @@ const peer = new Peer({
   host: "peerjs-server.herokuapp.com",
   port: 443,
   secure: true,
+  config: {
+    iceServers: [
+      { url: "stun:stun.l.google.com:19302" },
+      { url: "stun:stun1.l.google.com:19302" },
+    ],
+  },
+  fetch: {
+    mode: "no-cors",
+  },
 });
 
 export const createPeer = () => peer;
