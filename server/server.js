7b136eaab9825c425d53a239311a603fb208a5f7
Ushuaia Contenidos <agupeiretti@gmail.com>
2024-08-29 11:41:55 -0300

    sala de reunion

--- server/server.js
+++ server/server.js
@@ -1,43 +1,43 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
+// import express from "express";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+// import cors from "cors";
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5174",
-    methods: ["GET", "POST"],
-  },
-});
+// const server = createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5174",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  console.log("Nuevo cliente conectado");
+// io.on("connection", (socket) => {
+//   console.log("Nuevo cliente conectado");
 
-  socket.on("offer", (offer) => {
-    console.log("Oferta recibida:", offer);
-    socket.broadcast.emit("offer", offer);
-  });
+//   socket.on("offer", (offer) => {
+//     console.log("Oferta recibida:", offer);
+//     socket.broadcast.emit("offer", offer);
+//   });
 
-  socket.on("answer", (answer) => {
-    console.log("Respuesta recibida:", answer);
-    socket.broadcast.emit("answer", answer);
-  });
+//   socket.on("answer", (answer) => {
+//     console.log("Respuesta recibida:", answer);
+//     socket.broadcast.emit("answer", answer);
+//   });
 
-  socket.on("ice-candidate", (candidate) => {
-    console.log("Candidato ICE recibido:", candidate);
-    socket.broadcast.emit("ice-candidate", candidate);
-  });
+//   socket.on("ice-candidate", (candidate) => {
+//     console.log("Candidato ICE recibido:", candidate);
+//     socket.broadcast.emit("ice-candidate", candidate);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log("Cliente desconectado");
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("Cliente desconectado");
+//   });
+// });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () =>
-  console.log(`Servidor escuchando en el puerto ${PORT}`)
-);
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () =>
+//   console.log(`Servidor escuchando en el puerto ${PORT}`)
+// );
