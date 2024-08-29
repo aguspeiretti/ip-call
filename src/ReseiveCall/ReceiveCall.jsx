f5a429483b64dda2398b65c146fdfbba751307b1
Ushuaia Contenidos <agupeiretti@gmail.com>
2024-08-29 12:20:57 -0300

    sala de reunion

--- src/ReseiveCall/ReceiveCall.jsx
+++ src/ReseiveCall/ReceiveCall.jsx
@@ -1,14 +1,15 @@
-/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import { createPeer } from "../peerService";
 
 const ReceiveCall = () => {
   const [peerId, setPeerId] = useState(null);
-  const [streams, setStreams] = useState([]); // Almacenar múltiples streams
+  const [stream, setStream] = useState(null);
   const peer = createPeer();
 
   useEffect(() => {
+    // Obtenemos el ID del peer cuando se abre la conexión
     peer.on("open", (id) => {
+      console.log("Mi ID de Peer es:", id);
       setPeerId(id);
     });
 
@@ -16,10 +17,14 @@ const ReceiveCall = () => {
       navigator.mediaDevices
         .getUserMedia({ video: true, audio: true })
         .then((mediaStream) => {
+          setStream(mediaStream);
           call.answer(mediaStream);
 
           call.on("stream", (remoteStream) => {
-            setStreams((prevStreams) => [...prevStreams, remoteStream]); // Añadir nuevos streams
+            const videoElement = document.getElementById("remote-video");
+            if (videoElement) {
+              videoElement.srcObject = remoteStream;
+            }
           });
         })
         .catch((err) => console.error("Error al obtener media stream:", err));
@@ -28,19 +33,9 @@ const ReceiveCall = () => {
 
   return (
     <div>
-      <h3>Esperando llamadas... Mi ID es: {peerId}</h3>
-      <div>
-        {streams.map((stream, index) => (
-          <video
-            key={index}
-            autoPlay
-            style={{ width: "300px" }}
-            ref={(video) => {
-              if (video) video.srcObject = stream;
-            }}
-          />
-        ))}
-      </div>
+      <h3>Esperando llamadas... Mi ID es: {peerId}</h3>{" "}
+      {/* Mostramos el ID del Peer */}
+      <video id="remote-video" autoPlay style={{ width: "300px" }}></video>
     </div>
   );
 };
